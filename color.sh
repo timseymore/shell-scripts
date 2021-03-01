5ea0e86057b986916a8d0f17a482c2c0b6c5e19e
timseymore <tim_seymore2012@hotmail.com>
2021-03-01 00:05:39 -0500

    Added color.sh
    - will manage git color in the command line

---
+++ color.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Manages the colors in Git command line
+
+SCRIPT=$0
+OPT1=$1
+OPT2=$2
+
+case $OPT1 in
+    "")
+	echo "Usage: $SCRIPT [help|option] [option]"
+	exit 1
+	;;
+    *)
+	echo "NOT IMPLEMENTED"
+	exit 0
+	;;
+esac
