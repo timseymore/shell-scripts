87f600827bc519a6c36fc6303d04d84c3965e009
timseymore <tim_seymore2012@hotmail.com>
2021-03-02 06:39:50 -0500

    Updated color.sh

--- color.sh
+++ color.sh
@@ -6,13 +6,57 @@ SCRIPT=$0
 OPT1=$1
 OPT2=$2
 
+EXITCODE=0
+
+space() {
+    echo " "
+    return 0
+}
+
+show-usage() {
+    echo "Usage: $SCRIPT <option> <option> ..."
+    return 0
+}
+
+show-commands() {
+    echo " _________________________"
+    echo "|         Commands        |"
+    echo "|_________________________|"
+    echo "| help  | show help menu  |"
+    echo "|       |                 |"
+    echo "|_______|_________________|"
+    return 0
+}
+
+show-help() {
+    show-usage
+    space
+    show-commands
+    space
+    return 0
+}
+
+#TODO
+handle-args() {
+    echo "TODO"
+    return 2
+}
+
+# RUN
+
 case $OPT1 in
     "")
-	echo "Usage: $SCRIPT [help|option] [option]"
-	exit 1
+	show-usage
+	#$EXITCODE=1
+	;;
+    help)
+	show-help
+	#$EXITCODE=$?
 	;;
     *)
-	echo "NOT IMPLEMENTED"
-	exit 0
+	handle-args
+	#$EXITCODE=$?
 	;;
 esac
+
+exit 0
