a0325fd494a7878f93c99dc34433ad294479d849
timseymore <tim_seymore2012@hotmail.com>
2021-03-07 19:01:10 -0500

    Updated alias.sh

--- alias.sh
+++ alias.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-##############################
-# Manages Git alias settings #
-##############################
 
-
-# Global Variables
+# Manages Git alias settings
 
 SCRIPT="$0"
 FLAG="$1"
 
-
-
-# Functions
 
 function show-help() {
     echo "       Usage Flags"
@@ -22,7 +15,6 @@ function show-help() {
     echo "-h : Show help screen"
     return 0
 }
-
 
 function list-alias() {
     echo "Git Alias Settings"
@@ -66,7 +58,7 @@ case "$FLAG" in
 	git config --global alias.unset "config --global --unset"
 	git config --global alias.last "log -1 HEAD"
 	git config --global alias.unstage "reset HEAD--"
-	git config --global alias.lg "log --pretty=format:'%C(cyan dim)%h%C(green bold no-dim)%d%n%C(red dim)%D %C(white): %C(cyan)%an %C(yellow)<%ae>%n%C(white no-dim no-bold)%B' --graph --all"
+	git config --global alias.lg "log --pretty=format:'%C(cyan dim)%h%C(green bold no-dim)%d%n%C(red dim)%cr %C(white): %C(cyan)%an %C(yellow)<%ae>%n%C(white no-dim no-bold)%B' --graph --all"
 	git config --global alias.lgo "log --pretty=format:'%C(cyan dim)%h%C(green bold no-dim)%d %C(cyan bold): %C(red dim)%cr %C(cyan no-dim bold)-> %C(white no-bold)%s' --graph --all"
 	git config --global alias.ci commit
 	git config --global alias.ca "commit -a"
