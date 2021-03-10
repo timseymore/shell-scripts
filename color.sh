f0bb4c31e276c88d6e2b513c1d601711aa3336b4
Tim Seymore <tim_seymore2012@hotmail.com>
2021-03-10 18:47:34 -0500

    refactor: Update color.sh
    Update color.sh to have basic functionality.

--- color.sh
+++ color.sh
@@ -5,57 +5,75 @@
 SCRIPT=$0
 OPT1=$1
 OPT2=$2
+OPT3=$3
 
-EXITCODE=0
-
-space() {
-    echo " "
-    return 0
-}
-
-show-usage() {
+function show-usage() {
     echo "Usage: $SCRIPT <option> <option> ..."
     return 0
 }
 
-show-commands() {
-    echo " _________________________"
-    echo "|         Commands        |"
-    echo "|_________________________|"
-    echo "| help  | show help menu  |"
-    echo "|       |                 |"
-    echo "|_______|_________________|"
+function show-commands() {
+    echo " ___________________________________________________"
+    echo "|                       Commands                    |"
+    echo "|___________________________________________________|"
+    echo "| help <command>  | show help menu <help with cmd>  |"
+    echo "| set             | set to basic color scheme       |"
+    echo "| set <opt> <col> | set <opt> to color <col>        |"
+    echo "| list            | list current color settings     |"
+    echo "|_________________|_________________________________|"
     return 0
 }
 
-show-help() {
+function show-help() {
     show-usage
-    space
+    echo " "
     show-commands
-    space
+    echo" "
+    if ["$1" -ne ""]
+    then
+	echo "Showing help for ${1}:"
+	echo "TODO"
+    fi
     return 0
 }
 
-#TODO
-handle-args() {
-    echo "TODO"
-    return 2
+function set-color() {
+    local TARGET=$1
+    local COLOR=$2
+    echo "Setting color for $TARGET to $COLOR"
+    git --config --global color.$TARGET $COLOR && echo "Success" || echo "Failed" && return 1
+    return 0
 }
+
 
 # RUN
 
-case $OPT1 in
+case "$OPT1" in
     "")
 	show-usage
-	#$EXITCODE=1
 	;;
-    help)
-	show-help
-	#$EXITCODE=$?
+    "help")
+	show-help $OPT2
+	;;
+    "set")
+	if [ "$OPT2" -ne "" ]
+	then
+	    set-color $OPT2 $OPT3
+	else
+	    echo "Setting to basic color scheme..."
+	    echo "TODO"
+	    exit 1
+	fi	
+	;;
+    "list")
+	echo "Current Git Color Settings"
+	echo "=========================="
+	git config --list | grep color
+	echo "=========================="
 	;;
     *)
-	handle-args
-	#$EXITCODE=$?
+	echo "$OPT1 $OPT2"
+	exit 2
 	;;
 esac
 
