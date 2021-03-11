31c85a048116ccb04ec90b274e6256ddea8b3230
Tim Seymore <tim_seymore2012@hotmail.com>
2021-03-10 19:52:37 -0500

    refactor: Finish initial functionality of color.sh
    Finish the functionality of color.sh so that it can be useful.

--- color.sh
+++ color.sh
@@ -6,21 +6,22 @@ SCRIPT=$0
 OPT1=$1
 OPT2=$2
 OPT3=$3
+OPT4=$4
 
 function show-usage() {
-    echo "Usage: $SCRIPT <option> <option> ..."
+    echo "Usage: $SCRIPT <help|option1> <option2> <option3>"
     return 0
 }
 
 function show-commands() {
-    echo " ___________________________________________________"
-    echo "|                       Commands                    |"
-    echo "|___________________________________________________|"
-    echo "| help <command>  | show help menu <help with cmd>  |"
-    echo "| set             | set to basic color scheme       |"
-    echo "| set <opt> <col> | set <opt> to color <col>        |"
-    echo "| list            | list current color settings     |"
-    echo "|_________________|_________________________________|"
+    echo " __________________________________________________________________"
+    echo "|                            Commands                              |"
+    echo "|__________________________________________________________________|"
+    echo "| help <command>        | show help menu <help with cmd>           |"
+    echo "| set                   | set to basic color scheme                |"
+    echo "| set <tgt> <col> <sty> | set <opt> to color <col> and style <sty> |"
+    echo "| list                  | list current color settings              |"
+    echo "|_______________________|__________________________________________|"
     return 0
 }
 
@@ -29,7 +30,7 @@ function show-help() {
     echo " "
     show-commands
     echo" "
-    if ["$1" -ne ""]
+    if ["$1" != ""]
     then
 	echo "Showing help for ${1}:"
 	echo "TODO"
@@ -40,8 +41,9 @@ function show-help() {
 function set-color() {
     local TARGET=$1
     local COLOR=$2
-    echo "Setting color for $TARGET to $COLOR"
-    git --config --global color.$TARGET $COLOR && echo "Success" || echo "Failed" && return 1
+    local STYLE=$3
+    echo "Setting color for $TARGET to $COLOR $STYLE"
+    git config --global color.$TARGET $COLOR "$STYLE " && echo "Success" || echo "Failed" && return 1
     return 0
 }
 
@@ -56,13 +58,30 @@ case "$OPT1" in
 	show-help $OPT2
 	;;
     "set")
-	if [ "$OPT2" -ne "" ]
+	if [ "$OPT2" != "" ]
 	then
-	    set-color $OPT2 $OPT3
+	    set-color $OPT2 $OPT3 $OPT4
 	else
 	    echo "Setting to basic color scheme..."
-	    echo "TODO"
-	    exit 1
+	    set-color ui auto
+	    set-color interactive auto	    
+	    set-color branch auto
+	    set-color diff auto
+	    set-color status auto
+	    
+	    set-color branch.current cyan
+	    set-color branch.local yellow
+	    set-color branch.remote green
+	    
+	    set-color diff.meta yellow
+	    set-color diff.frag magenta
+	    set-color diff.old red
+	    set-color diff.new green
+	    
+	    set-color status.added green
+	    set-color status.changed blue
+	    set-color status.untracked magenta
+	   
 	fi	
 	;;
     "list")
