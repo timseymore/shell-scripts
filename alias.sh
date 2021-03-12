4fe83a648d7685ea60042da356b3e45568f091cb
Tim Seymore <tim_seymore2012@hotmail.com>
2021-03-12 00:12:43 -0500

    refactor: Refactor alias.sh to use set-alias() function
    Implement the se-alias() function to more easily set an alias.

--- alias.sh
+++ alias.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
 # Manages Git alias settings
 
 SCRIPT="$0"
 FLAG="$1"
-
 
 function show-help() {
     echo "       Usage Flags"
@@ -26,8 +24,11 @@ function list-alias() {
 }
 
 function set-alias() {
-        
+    local NAME=$1
+    local VALUE=$2
 
+    echo "Setting 'git ${1}' to 'git ${2}'."
+    git config -- global "alias.${VALUE}" "$NAME" && echo Success || echo "Failed: $1 not set" 
     return 0
 }
 
@@ -53,33 +54,33 @@ case "$FLAG" in
 	;;
     -s)
 	echo "Setting up Git alias..."
-	git config --global alias.settings "config --list"
-	git config --global alias.set "config --global"
-	git config --global alias.unset "config --global --unset"
-	git config --global alias.last "log -1 HEAD"
-	git config --global alias.unstage "reset HEAD--"
-	git config --global alias.lg "log --pretty=format:'%C(cyan dim)%h%C(green bold no-dim)%d%n%C(red dim)%cr %C(white): %C(cyan)%an %C(yellow)<%ae>%n%C(white no-dim no-bold)%B' --graph --all"
-	git config --global alias.lgo "log --pretty=format:'%C(cyan dim)%h%C(green bold no-dim)%d %C(cyan bold): %C(red dim)%cr %C(cyan no-dim bold)-> %C(white no-bold)%s' --graph --all"
-	git config --global alias.ci commit
-	git config --global alias.ca "commit -a"
-	git config --global alias.cm "commit -m"
-	git config --global alias.cam "commit -a -m"
-	git config --global alias.st status
-	git config --global alias.br branch
-	git config --global alias.bv "branch -vv"
-	git config --global alias.mkbr "checkout -b"
-	git config --global alias.rmbr "branch -d"
-	git config --global alias.rmbr-h "branch -D"
-	git config --global alias.co checkout
-	git config --global alias.track "checkout --track"
-	git config --global alias.rs "remote show"
-	git config --global alias.rv "remote -v"
-	git config --global alias.mkrm "remote add"
-	git config --global alias.delrm "remote rm"
-	git config --global alias.mvrm "remote rename"
-	git config --global alias.ta "tag -a"
-	git config --global alias.count "count-objects -Hv"
-	echo "Finished setting alias."
+	set-alias settings "config --list"
+	set-alias set "config --global"
+	set-alias unset "config --global --unset"
+	set-alias last "log -1 HEAD"
+	set-alias unstage "reset HEAD--"
+	set-alias lg "log --pretty=format:'%C(cyan dim)%h%C(green bold no-dim)%d%n%C(red dim)%cr %C(white): %C(cyan)%an %C(yellow)<%ae>%n%C(white no-dim no-bold)%B' --graph --all"
+	set-alias lgo "log --pretty=format:'%C(cyan dim)%h%C(green bold no-dim)%d %C(cyan bold): %C(red dim)%cr %C(cyan no-dim bold)-> %C(white no-bold)%s' --graph --all"
+	set-alias ci commit
+	set-alias ca "commit -a"
+	set-alias cm "commit -m"
+	set-alias cam "commit -a -m"
+	set-alias st status
+	set-alias br branch
+	set-alias bv "branch -vv"
+	set-alias mkbr "checkout -b"
+	set-alias rmbr "branch -d"
+	set-alias rmbr-h "branch -D"
+	set-alias co checkout
+	set-alias track "checkout --track"
+	set-alias rs "remote show"
+	set-alias rv "remote -v"
+	set-alias mkrm "remote add"
+	set-alias delrm "remote rm"
+	set-alias mvrm "remote rename"
+	set-alias ta "tag -a"
+	set-aias count "count-objects -Hv"
+	echo "Finished setting all basic git alias."
 	list-alias
 	;;
     *)
