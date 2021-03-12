8e316904bf51836d0efea015165ba891a4e43123
Tim Seymore <tim_seymore2012@hotmail.com>
2021-03-12 01:40:07 -0500

    feat: Add ability to use mvtg.sh without a message arg
    Add the ability to use mvtg.sh without a message arg to be able to use
    the editor for the message.

--- scripts/git/mvtg.sh
+++ scripts/git/mvtg.sh
@@ -7,10 +7,20 @@ TAG=$1
 COMMIT=$2
 MESSAGE=$3
 
-if [$TAG != "" && $COMMIT != "" && $MESSAGE != ""]
+if [$TAG != "" && $COMMIT != ""]
 then
-    
-    git tag -a $TAG -f $COMMIT -m "$MESSAGE"
+    if [$MESSAGE != ""]
+    then
+	echo "Moving $TAG to $COMMIT with message:"
+	echo "$MESSAGE"
+	git tag -a $TAG -f $COMMIT -m "$MESSAGE" && echo "Success" || echo "Failed to move $TAG" && exit 2
+    else
+	echo "Moving $TAG to $COMMIT without message."
+	echo "Opening editor..."
+	git tag -a $TAG -f $COMMIT && echo "Success" || echo "Failed to move $TAG" && exit 2
+    fi
+    exit 0
 else
-    echo "Usage: $SCRIPT [tag] [commit] [message]"
+    echo "Usage: $SCRIPT <tag> <commit> |Optional: <message>|"
+    exit 1
 fi
