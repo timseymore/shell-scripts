a621c3ac9e93b44037951d3cc8c894d00af009ed
Tim Seymore <tim_seymore2012@hotmail.com>
2021-03-15 22:18:06 -0400

    refactor: Update mvtg.sh
     - Update conditional branching to better handle various argument
       conditions.
     - Add usage message variable to keep code clean and manageable.

--- scripts/git/mvtg.sh
+++ scripts/git/mvtg.sh
@@ -6,21 +6,29 @@ SCRIPT=$0
 TAG=$1
 COMMIT=$2
 MESSAGE=$3
+USAGEMSG="Usage: $SCRIPT <tag> <commit> |Optional <message>|"
 
-if [$TAG != "" && $COMMIT != ""]
+if [ "$TAG" != "" ]
 then
-    if [$MESSAGE != ""]
-    then
-	echo "Moving $TAG to $COMMIT with message:"
-	echo "$MESSAGE"
-	git tag -a $TAG -f $COMMIT -m "$MESSAGE" && echo "Success" || echo "Failed to move $TAG" && exit 2
+    if [ "$COMMIT" != "" ]
+    then	
+	if [ "$MESSAGE" != "" ]
+	then
+	    echo "Moving $TAG to $COMMIT with message:"
+	    echo "$MESSAGE"
+	    git tag -a $TAG -f $COMMIT -m "$MESSAGE" && echo "Success" || echo "Failed to move $TAG" && exit 3
+	else
+	    echo "Moving $TAG to $COMMIT without message."
+	    echo "Opening editor..."
+	    git tag -a $TAG -f $COMMIT && echo "Success" || echo "Failed to move $TAG" && exit 3
+	fi
+	exit 0
     else
-	echo "Moving $TAG to $COMMIT without message."
-	echo "Opening editor..."
-	git tag -a $TAG -f $COMMIT && echo "Success" || echo "Failed to move $TAG" && exit 2
-    fi
-    exit 0
+	echo "Missing commit."
+	echo "$USAGEMSG"
+	exit 2
+    fi    
 else
-    echo "Usage: $SCRIPT <tag> <commit> |Optional: <message>|"
+    echo "$USAGEMSG"
     exit 1
 fi
